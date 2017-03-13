286c79e4b0a039c42d612fb88540ddcca3f655a8
Ken <kenjyco@gmail.com>
2017-03-13 09:32:08 -0500

    Bump to v0.2.1

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open('README.rst', 'r') as fp:
 
 setup(
     name='chloop',
-    version='0.2.0',
+    version='0.2.1',
     description='A Redis-backed REPL that saves command history, output, & errors',
     long_description=long_description,
     author='Ken',
     author_email='kenjyco@gmail.com',
     license='MIT',
     url='https://github.com/kenjyco/chloop',
-    download_url='https://github.com/kenjyco/chloop/tarball/v0.2.0',
+    download_url='https://github.com/kenjyco/chloop/tarball/v0.2.1',
     packages=['chloop'],
     install_requires=[
         'click>=6.0',
@@ -23,6 +23,7 @@ setup(
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
+        'Environment :: Console',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.5',
